9603e91d7103c47776516d406eb0c4269f46e6f5
Muhammad Saim <141134993+saim-x@users.noreply.github.com>
2023-08-01 15:54:43 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 import pyautogui as py
-
+user = input("Which channel you want to subscribe? \n enter the name : ")
 py.press('win')
 time.sleep(0.5)
 py.write('chrome')
@@ -28,17 +28,68 @@ py.press('enter')
 
 def move_cursor_to_image(image_path):
     try:
-        location = py.locateOnScreen(image_path, confidence=0.5)
+        location = py.locateOnScreen(image_path, confidence=0.6)
         if location is not None:
-            x, y, width, height = py.center(location)
+            x, y = py.center(location)
             py.moveTo(x, y)
             print("Image found and cursor moved to the center of the image.")
         else:
             print("Image not found on the screen.")
+            print(location)
     except py.ImageNotFoundException:
         print("Image not found on the screen.")
+        print(location)
 
 # Provide the path to the image you want to search for
-image_path = "D://Projects//first sample//search.png"
+image_path = "t2.png"
+time.sleep(1.5)
 move_cursor_to_image(image_path)
 py.click()
+time.sleep(0.9)
+py.write(user)
+py.press('enter')
+def move_cursor_to_image1(image_path_1):
+    try:
+        location = py.locateOnScreen(image_path_1, confidence=0.6)
+        if location is not None:
+            x, y = py.center(location)
+            py.moveTo(x, y)
+            print("Image found and cursor moved to the center of the image.")
+            py.click()
+            time.sleep(1)
+            py.click()
+        else:
+            print("Image not found on the screen.")
+            print(location)
+    except py.ImageNotFoundException:
+        print("Image not found on the screen.")
+        print(location)
+
+# Provide the path to the image you want to search for
+image_path_1 = "t3.png"
+time.sleep(1.5)
+move_cursor_to_image1(image_path_1)
+
+
+
+
+def move_cursor_to_image2(image_path_2):
+    try:
+        location = py.locateOnScreen(image_path_2, confidence=0.9)
+        if location is not None:
+            x, y = py.center(location)
+            py.moveTo(x, y)
+            print("Image found and cursor moved to the center of the image.")
+            py.click()
+        else:
+            print("Image not found on the screen.")
+            print(location)
+    except py.ImageNotFoundException:
+        print("Image not found on the screen.")
+        print(location)
+
+# Provide the path to the image you want to search for
+image_path_2 = "t5.png"
+time.sleep(1.5)
+move_cursor_to_image2(image_path_2)
+
